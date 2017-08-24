693925297816a7f75e4145f7f457d5455f09a488
mhanelia <murilo.aippe@hotmail.com>
2017-08-24 16:40:11 -0300

    classe simples e com metodo metodo

---
+++ classePessoa.py
@@ -0,0 +1,22 @@
+# Classe pessoa com o método fala
+
+class Pessoa(object):
+    #Os métodos da classe sempre receberão self como parâmetro
+
+    #Método construtor da classe, recebe nome e idade
+    def __init__(self, nome, idade):
+
+        #Inicializa as variáveis que serão utilizadas
+        #pelos métodos da classe Pessoa (e somente por eles)
+        self.nome = nome
+        self.idade = idade
+
+    #Método fala
+    def fala(self):
+        print("Olá! Eu sou {}, tenho {} anos e estou falando!".format(self.nome, self.idade))
+
+###################################
+
+#Instanciando a classe Pessoa, utilizando um objeto chamado Pessoa
+#desta forma, pessoa (diferente de Pessoa), passa a ser um obejto
+#de uma classe. Vejamos:
