8a98e36a056c677b13036b9ce56c478fc5cc739a
mhanelia <murilo.aippe@hotmail.com>
2017-08-24 13:51:34 -0300

    Corrigido nomes das strings e detalhes

--- Ping.py
+++ Ping.py
@@ -3,12 +3,12 @@ from os import system as system_call       # Executa o shell no comando
 
 def ping(host):
     """
-    Retorna True se o host (str) rsponde a requisição do ping.
+    Retorna True se o host (str) responde a requisição do ping.
     Lembre-se de que alguns hosts podem não responder a um pedido de ping mesmo se o nome do host for válido.
     """
 
-    # Parâmetros Ping como função do SO
-    parameters = "-n 1" if system_name().lower()=="windows" else "-c 1"
+    # Parâmetros do comando Ping como função do SO: -n numero de solicitações (1 por padrão)
+    parameters = "-n 1" if sys_name().lower()=="windows" else "-c 1"
 
     # Pingando
     return system_call("ping " + parameters + " " + host) == 0
