445ed2dac8d70c1abfed1026462846ff3a1a8f41
mhanelia <murilo.aippe@hotmail.com>
2017-08-24 14:19:40 -0300

    trabalhando com list e for simples

---
+++ concatStr.py
@@ -0,0 +1,5 @@
+list = [] #Cria uma lista vazia
+for i in range(5): #for passando pelo range definido em 5x
+    name= input("Nome: ") # Recebe uma entrada de dados e salva na variavel name
+    list.append(name) #Pega o que foi inserido na variavel name e adiciona a lista como um novo indice
+print(list) #Exibe a lista
