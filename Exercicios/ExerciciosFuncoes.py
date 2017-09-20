ddfb2b6937898d2ad093e704aeee74903836dd39
mhanelia <murilo.aippe@hotmail.com>
2017-09-20 15:20:10 -0300

    update

--- Exercicios/ExerciciosFuncoes.py
+++ Exercicios/ExerciciosFuncoes.py
@@ -28,22 +28,73 @@ imprima até a n-ésima linha.
 
 --------------------------------------------------------------------------------
 
-Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
-Faça um programa, com uma função que necessite de um argumento. A função retorna o valor de caractere ‘P’, se seu argumento for positivo, e ‘N’, se seu argumento for zero ou negativo.
-Faça um programa com uma função chamada somaImposto. A função possui dois parâmetros formais: taxaImposto, que é a quantia de imposto sobre vendas expressa em porcentagem e custo, que é o custo de um item antes do imposto. A função “altera” o valor de custo para incluir o imposto sobre vendas.
-Faça um programa que converta da notação de 24 horas para a notação de 12 horas. Por exemplo, o programa deve converter 14:25 em 2:25 P.M. A entrada é dada em dois inteiros. Deve haver pelo menos duas funções: uma para fazer a conversão e uma para a saída. Registre a informação A.M./P.M. como um valor ‘A’ para A.M. e ‘P’ para P.M. Assim, a função para efetuar as conversões terá um parâmetro formal para registrar se é A.M. ou P.M. Inclua um loop que permita que o usuário repita esse cálculo para novos valores de entrada todas as vezes que desejar.
+# Faça um programa, com uma função que necessite de três argumentos, e que forneça
+# a soma desses três argumentos.
+def soma(a,b,c):
+    return a+b+c
+--------------------------------------------------------------------------------
+
+# Faça um programa, com uma função que necessite de um argumento. A função retorna
+# o valor de caractere ‘P’, se seu argumento for positivo, e ‘N’, se seu argumento
+# for zero ou negativo.
+def argumento(n):
+    if n > 0:
+        return "P"
+    else:
+        return "N"
+--------------------------------------------------------------------------------
+
+Faça um programa com uma função chamada somaImposto. A função possui dois
+parâmetros formais: taxaImposto, que é a quantia de imposto sobre vendas expressa
+em porcentagem e custo, que é o custo de um item antes do imposto. A função
+“altera” o valor de custo para incluir o imposto sobre vendas.
+
+--------------------------------------------------------------------------------
+
+Faça um programa que converta da notação de 24 horas para a notação de 12 horas.
+Por exemplo, o programa deve converter 14:25 em 2:25 P.M. A entrada é dada em
+dois inteiros. Deve haver pelo menos duas funções: uma para fazer a conversão
+e uma para a saída. Registre a informação A.M./P.M. como um valor ‘A’ para A.M.
+e ‘P’ para P.M. Assim, a função para efetuar as conversões terá um parâmetro
+formal para registrar se é A.M. ou P.M. Inclua um loop que permita que o
+usuário repita esse cálculo para novos valores de entrada todas as vezes que desejar.
+
+--------------------------------------------------------------------------------
 Faça um programa que use a função valorPagamento para determinar o valor a ser pago por uma prestação de uma conta. O programa deverá solicitar ao usuário o valor da prestação e o número de dias em atraso e passar estes valores para a função valorPagamento, que calculará o valor a ser pago e devolverá este valor ao programa que a chamou. O programa deverá então exibir o valor a ser pago na tela. Após a execução o programa deverá voltar a pedir outro valor de prestação e assim continuar até que seja informado um valor igual a zero para a prestação. Neste momento o programa deverá ser encerrado, exibindo o relatório do dia, que conterá a quantidade e o valor total de prestações pagas no dia. O cálculo do valor a ser pago é feito da seguinte forma. Para pagamentos sem atraso, cobrar o valor da prestação. Quando houver atraso, cobrar 3% de multa, mais 0,1% de juros por dia de atraso.
 Faça uma função que informe a quantidade de dígitos de um determinado número inteiro informado.
 ----------------------------------------------------------------------------------------
-Reverso do número. Faça uma função que retorne o reverso de um número inteiro informado. Por exemplo: 127 -> 721.
+# Reverso do número. Faça uma função que retorne o reverso de um número inteiro informado. Por exemplo: 127 -> 721.
 def reverte(n):
-    return = int(str(n)[::-1])
+    return int(str(n)[::-1])
 
 ----------------------------------------------------------------------------------------
 
-Jogo de Craps. Faça um programa de implemente um jogo de Craps. O jogador lança um par de dados, obtendo um valor entre 2 e 12. Se, na primeira jogada, você tirar 7 ou 11, você um "natural" e ganhou. Se você tirar 2, 3 ou 12 na primeira jogada, isto é chamado de "craps" e você perdeu. Se, na primeira jogada, você fez um 4, 5, 6, 8, 9 ou 10,este é seu "Ponto". Seu objetivo agora é continuar jogando os dados até tirar este número novamente. Você perde, no entanto, se tirar um 7 antes de tirar este Ponto novamente.
+Jogo de Craps. Faça um programa de implemente um jogo de Craps. O jogador lança
+um par de dados, obtendo um valor entre 2 e 12. Se, na primeira jogada, você
+tirar 7 ou 11, você um "natural" e ganhou. Se você tirar 2, 3 ou 12 na primeira
+jogada, isto é chamado de "craps" e você perdeu. Se, na primeira jogada, você
+fez um 4, 5, 6, 8, 9 ou 10,este é seu "Ponto". Seu objetivo agora é continuar
+jogando os dados até tirar este número novamente. Você perde, no entanto, se
+tirar um 7 antes de tirar este Ponto novamente.
 
-Data com mês por extenso. Construa uma função que receba uma data no formato DD/MM/AAAA e devolva uma string no formato D de mesPorExtenso de AAAA. Opcionalmente, valide a data e retorne NULL caso a data seja inválida.
+----------------------------------------------------------------------------------------
+
+# Data com mês por extenso. Construa uma função que receba uma data no formato
+# DD/MM/AAAA e devolva uma string no formato D de mesPorExtenso de AAAA.
+# Opcionalmente, valide a data e retorne NULL caso a data seja inválida.
+
+def extenso(data):
+    data = "05/11/1991"
+    meses = {"01": " de janeiro de ", "02": "de fevereirode ", "03": " de março de ", "04": " de abril de ", "05": " de maio de ",
+"06": " de junho de ", "07": " de julho de ", "08": " de agosto de ", "09": " de setembro de ", '10': " de outubro de " ,"11": " de novembro de ",
+"12": " de dezembro de "}
+
+    list = data.split("/")
+
+    list[1] = meses[str(list[1])]
+
+    data = ''.join(list)
+    return data
 
 ----------------------------------------------------------------------------------------
 Embaralha palavra. Construa uma função que receba uma string como parâmetro e
@@ -51,6 +102,8 @@ devolva outra string com os carateres embaralhados. Por exemplo: se função rec
 a palavra python, pode retornar npthyo, ophtyn ou qualquer outra combinação possível,
 de forma aleatória. Padronize em sua função que todos os caracteres serão devolvidos
 em caixa alta ou caixa baixa, independentemente de como foram digitados.
+
+
 
 ----------------------------------------------------------------------------------------
 
