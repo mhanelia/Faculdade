5fd2d07c445cc0b9d25676f2b8d965c9f7431514
mhanelia <murilo.aippe@hotmail.com>
2017-09-25 22:44:50 -0300

    Add files via upload

---
+++ Black Jack/__main__.py
@@ -0,0 +1,22 @@
+"""Arquivo princial."""
+from black_jack import BlackJack
+
+b = BlackJack()
+b.shuffle()
+pc_hand = b.two_first_cards()
+player_hand = b.two_first_cards()
+
+while (b.get_point(player_hand) <= 16):
+    aux = input("Deseja comprar mais alguma carta? s/n")
+    if "s" in aux:
+        pc_hand = b.hit(pc_hand)
+        b.show_hand(player_hand, b.get_point(player_hand))
+    else:
+        b.show_hand(player_hand, b.get_point(player_hand))
+    break
+
+while (b.get_point(pc_hand) <= 16):
+        pc_hand = b.hit(pc_hand)
+        b.show_hand(pc_hand, b.get_point(pc_hand))
+
+b.bet(pc_hand, b.get_point(pc_hand))
