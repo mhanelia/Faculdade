abf57282ff991316aa7914c4742732587c132724
mhanelia <8427316+mhanelia@users.noreply.github.com>
2017-10-17 15:19:38 -0300

    Update fodinha.py

--- Fodinha/fodinha.py
+++ Fodinha/fodinha.py
@@ -13,16 +13,14 @@ class Fodinha(object):
         self.suits =   ['0', "♦", "♠", "♥", "♣"]
         self.appendHand = []
         self.deck = self.create_deck()
-        self.round = 1
+        self.round = 3
 
 
     def create_deck(self):
          """Método que cria o baralho"""
-         numbers = ["4", "5", "6", "7", "Q", "J", "K", "A", "2", "3"]
-         suits =   ["♦", "♠", "♥", "♣"]
          deck = []
-         for i in numbers:
-             for x in suits:
+         for i in self.numbers[1:]:
+             for x in self.suits[1:]:
                  deck.append(i+x)
 
          return deck
@@ -44,53 +42,70 @@ class Fodinha(object):
         """Incrementa o round para a próxima jogada"""
         self.round = self.round + 1
 
-    def table(self):
+    def f_table(self):
         """Define a carta que estará na mesa"""
         self.table = []
         self.table.append(self.deck[:1])
         del self.deck[:1]
-        return self.table
 
 
     def f_appendHand(self, mao):
+        """Adiciona a primeira carta da mão ao jogo"""
         self.appendHand.append(mao)
 
-    def compare(self):
 
+    def compare(self):
+        """Realiza a comparação dos pesos das cartas"""
+
+        self.table = self.table[0]
+        self.table = self.table[0]
+        print(self.appendHand)
+        lista = []
+        lista2= []
         for itens in self.appendHand:
             for n in itens:
+
+                lista2.append(n)
                 if '♦' in n[1]:
-                    if numbers.index(n[0]) == numbers.index(self.table[0]) + 1 :
-                        res = ((ord('♦') - 7)) * numbers.index(n[0]) * 2000
+                    if (n[0] == '4' and self.table[0] == '3') or self.numbers.index(n[0]) == self.numbers.index(self.table[0]) + 1 :
+                        res = ((ord('♦') - 7)) * self.numbers.index(n[0]) * 2000
                         lista.append(res)
                     else:
-                        res = ((ord('♦') - 7)) * numbers.index(n[0])
+                        res = ((ord('♦') - 7)) * self.numbers.index(n[0])
                         lista.append(res)
 
                 elif '♠' in n[1]:
-                        if numbers.index(n[0]) == numbers.index(self.table[0]) + 1 :
-                            res = (ord('♠')) * numbers.index(n[0]) * 2000
+                        if (n[0] == '4' and self.table[0] == '3') or self.numbers.index(n[0]) == self.numbers.index(self.table[0]) + 1 :
+                            res = (ord('♠')) * self.numbers.index(n[0]) * 2000
                             lista.append(res)
                         else:
-                            res = (ord('♠')) * numbers.index(n[0])
+                            res = (ord('♠')) * self.numbers.index(n[0])
                             lista.append(res)
 
                 elif '♥' in n[1]:
-                    if numbers.index(n[0]) == numbers.index(self.table[0]) + 1 :
-                        res = ((ord('♥') - 4)) * numbers.index(n[0]) * 2000
+                    if (n[0] == '4' and self.table[0] == '3') or self.numbers.index(n[0]) == self.numbers.index(self.table[0]) + 1 :
+                        res = ((ord('♥') - 4)) * self.numbers.index(n[0]) * 2000
                         lista.append(res)
                     else:
-                        res = ((ord('♥') - 4)) * numbers.index(n[0])
+                        res = ((ord('♥') - 4)) * self.numbers.index(n[0])
                         lista.append(res)
 
                 elif '♣' in n[1]:
-                    if numbers.index(n[0]) == numbers.index(self.table[0]) + 1 :
-                        res = (ord('♣') - 1) * numbers.index(n[0]) * 2000
+                    if (n[0] == '4' and self.table[0] == '3') or self.numbers.index(n[0]) == self.numbers.index(self.table[0]) + 1 :
+                        res = (ord('♣') - 1) * self.numbers.index(n[0]) * 2000
                         lista.append(res)
                     else:
-                        res = (ord('♣') - 1) * numbers.index(n[0])
+                        res = (ord('♣') - 1) * self.numbers.index(n[0])
                         lista.append(res)
 
-        best = appendHand[lista.index(max(lista))]
+        print("Mesa: {}".format(self.table))
+        print("Mão 1: {} ---- Mão 2: {}".format(self.appendHand[0], self.appendHand[1]))
+        best = lista2[lista.index(max(lista))]
 
-        print("Melhor carta: {}".format(best))
+        print("\nMelhor carta: {}".format(best))
+
+        del self.appendHand[0]
+
+
+    def r_hand(self):
+        """Realiza o descarte da carta jogada"""
